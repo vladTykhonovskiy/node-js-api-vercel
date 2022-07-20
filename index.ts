dd3b1d58200195ce419c570a91c62b82f3a8bf0b
vladtihonovskiy <v.tykhonovskiy@softpositive.com>
2022-07-20 13:25:50 +0300

    fix spell issue

--- index.ts
+++ index.ts
@@ -8,11 +8,11 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server testß');
+  res.send('Express + TypeScript Server test');
 });
 
 app.use('/api/', routes);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at ßhttp://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
